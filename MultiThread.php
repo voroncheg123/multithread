24603a811b254564ef653b2fe213f64e3cb33cdf
Andrew Vorona <voroncheg@gmail.com>
2022-09-26 22:36:45 +0300

    Add comments to methods

--- MultiThread.php
+++ MultiThread.php
@@ -10,6 +10,15 @@ class MultiThread
     private $iteration;
     private $separator;
 
+    /**
+     * Creates thread
+     * @param $params
+     * @param $handler
+     * @param $iteration
+     * @param $separator
+     * @return void
+     */
+
     public function execute($params, $handler, $iteration,$separator){
 
         $this->iteration = $iteration;
@@ -20,6 +29,11 @@ class MultiThread
         $this->run();
     }
 
+    /**
+     * Starts a thread in the background
+     * @return void
+     */
+
     private function run(){
 
         $cmd = 'php '.$this->handler.' '.$this->pull.' > '.$this->log_file.' 2>&1 &';
@@ -27,9 +41,21 @@ class MultiThread
         exec($cmd);
     }
 
+    /**
+     * Sets handler file
+     * @param $file_name string
+     * @return void
+     */
+
     private function set_handler($file_name){
         $this->handler = __DIR__.'/handlers/'.$file_name;
     }
+
+    /**
+     * Sets and creates log file
+     * @param $handler string
+     * @return void
+     */
 
     private function set_log_file($handler){
         $post_fix = $this->iteration.'.log';
@@ -38,6 +64,11 @@ class MultiThread
         }
         $this->log_file = __DIR__.'/logs/'.str_replace('.php',$post_fix, $handler);
     }
+
+    /** Creates a string to pass to the thread
+     * @param $params
+     * @return void
+     */
 
     private function set_pull($params){
         $pull_str = '';
